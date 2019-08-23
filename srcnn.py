c44dd8640b440dcf5b341128a5847ec44e064092
mistralk <sohn.yunha@gmail.com>
2019-08-23 23:30:47 +0900

    train and test

--- srcnn.py
+++ srcnn.py
@@ -23,11 +23,12 @@ class ImageDataset(Dataset):
         img = Image.open(img_name).convert('RGB')
 
         if self.transform is not None:
-            gt = self.transform(img)
+            img = self.transform(img)
+        gt = img
 
         downsample = transforms.Compose([
-            transforms.Resize(112, Image.LANCZOS),
-            transforms.Resize(224, Image.LANCZOS),
+            transforms.Resize(64, Image.BICUBIC),
+            transforms.Resize(128, Image.BICUBIC),
             transforms.ToTensor()])
 
         lowres = downsample(gt)
@@ -43,11 +44,12 @@ def load_dataset(root_dir):
     img_paths = [str(path) for path in img_paths]
     
     transform = transforms.Compose([
-        transforms.CenterCrop(224)])
+        transforms.CenterCrop(400),
+        transforms.RandomCrop(128)])
     
     train_dataset = ImageDataset(root_dir, img_paths, transform)  
-    train_dataloader = DataLoader(train_dataset, batch_size=64, 
-                                  shuffle=True, num_workers=6)
+    train_dataloader = DataLoader(train_dataset, batch_size=128, 
+                                  shuffle=True, num_workers=12)
     
     print(len(train_dataset), 'image paths imported from', root_dir)
 
@@ -63,10 +65,27 @@ def load_dataset(root_dir):
             break
 '''
 
+def load_testset(root_dir):
+    root_dir = pathlib.Path(root_dir)
+    img_paths = list(root_dir.glob('**/*.*'))
+    img_paths = [str(path) for path in img_paths]
+    
+    transform = transforms.Compose([
+        transforms.CenterCrop(128)])
+    
+    test_dataset = ImageDataset(root_dir, img_paths, transform=transform)
+    test_dataloader = DataLoader(test_dataset, batch_size=len(test_dataset), 
+                                  shuffle=False, num_workers=1)
+    
+    print(len(test_dataset), 'test image paths imported from', root_dir)
+
+    return test_dataloader
+
+
 def train(dataloader):
     device = torch.device('cuda')
 
-    n_epochs = 10
+    n_epochs = 30
     
     model = torch.nn.Sequential(
         torch.nn.ZeroPad2d(4),
@@ -84,10 +103,11 @@ def train(dataloader):
     model.to(device)
 
     loss_fn = torch.nn.MSELoss().to(device)
-    learning_rate = 1e-4
+    learning_rate = 0.001
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     
     for epoch in range(n_epochs):
+        avg_loss = 0.0
         for i_batch, batch in enumerate(dataloader):
             x = batch['lowres'].to(device)
             y = batch['label'].to(device)
@@ -98,27 +118,60 @@ def train(dataloader):
             loss = loss_fn(y_pred, y)
             loss.backward()
             optimizer.step()
+            
+            avg_loss += loss
+            print('Epoch {}({}/{}) - Loss:{:.6f}'.format(epoch, 
+                        i_batch, len(dataloader), loss.item()))
+        
+        avg_loss /= len(dataloader)
+        avg_psnr = 10 * math.log10(1 / avg_loss)
+        print('Epoch {} average - Loss:{:.6f} PSNR:{}'.format(
+                    epoch, avg_loss, avg_psnr))
+
+        save_model(model, epoch)
     
-            psnr = 10 * math.log10(1 / loss.item())
-
-            print('Epoch {}({}/{}) - Loss:{:.6f} PSNR:{:.6f}'.format(epoch, 
-                        i_batch, len(dataloader), loss.item(), psnr))
-
-    return 0
+    return model
 
 
 def valid():
     return 0
 
 
-def test():
-    return 0
+def test(model, dataloader):
+    device = torch.device('cuda')
+    loss_fn = torch.nn.MSELoss().to(device)
+    avg_psnr = 0.0
+
+    with torch.no_grad():
+        for batch in dataloader:
+            x = batch['lowres'].to(device)
+            y = batch['label'].to(device)
+
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            psnr = 10 * math.log10(1 / loss.item())
+            avg_psnr += psnr
+
+    print('Test set - Average PSNR:{}'.format(avg_psnr))
+
+
+def use(model_path):
+    device = torch.device('cuda')
+
+
+def save_model(model, epoch):
+    path = 'model_{}.pth'.format(epoch)
+    torch.save(model, path)
+    print(path, 'saved')
 
 
 def main():
     train_dataloader = load_dataset('dataset/')
-    train(train_dataloader)
-    
+    test_dataloader = load_testset('testset/')
+
+    model = train(train_dataloader)
+    test(model, test_dataloader)
+
 
 if __name__ == '__main__':
     main()
